a8bd6ee480014c4a6e69e93b89bb35cff8e95ec2
kcreed <kcreed@cox.net>
2016-01-24 00:35:39 -0500

    added fields to Elevator

--- src/com/elevator/elevator/Elevator.java
+++ src/com/elevator/elevator/Elevator.java
@@ -8,5 +8,40 @@ package com.elevator.elevator;
  *
  */
 public class Elevator {
+	
+	int floorMax = 0;
+	int floorMin = 0;
+	int currentFloor = 0;
+	
+	public Elevator() {
+		
+	}
+
+	public int getFloorMax() {
+		return floorMax;
+	}
+
+	public void setFloorMax(int floorMax) {
+		this.floorMax = floorMax;
+	}
+
+	public int getFloorMin() {
+		return floorMin;
+	}
+
+	public void setFloorMin(int floorMin) {
+		this.floorMin = floorMin;
+	}
+
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public void setCurrentFloor(int currentFloor) {
+		this.currentFloor = currentFloor;
+	}
+	
+	
+	
 
 }
