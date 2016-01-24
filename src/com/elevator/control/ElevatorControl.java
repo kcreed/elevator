bb273517b0208b3850d8df94ec73c6c378bd3536
kcreed <kcreed@cox.net>
2016-01-24 00:06:44 -0500

    initial commit

---
+++ src/com/elevator/control/ElevatorControl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.elevator.control;
+
+/**
+ * @author Kevin
+ *
+ */
+public class ElevatorControl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
